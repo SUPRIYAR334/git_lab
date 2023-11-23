8e1a159e8f7f633bd7813100f7adfdfdef717f9b
SUPRIYAR334 <111135178+SUPRIYAR334@users.noreply.github.com>
2023-11-23 10:35:03 +0530

    Update Gti.java

--- Gti.java
+++ Gti.java
@@ -2,8 +2,6 @@ class Gti
 {
   public static void main(String[] argd)
   {
-      System.out.println("hello Git");
-      System.out.println("hello Git");
-      System.out.println("hello Git");
+      
   }
 }
