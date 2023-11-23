dee6cd5a03420a10dbe62275a092e1a375071ad1
SUPRIYAR334 <111135178+SUPRIYAR334@users.noreply.github.com>
2023-11-23 10:43:15 +0530

    Update Gti.java in master

--- Gti.java
+++ Gti.java
@@ -2,7 +2,7 @@ class Gti
 {
   public static void main(String[] argd)
   {
-      System.out.println("hello Git");
+      System.out.println("xxxxxx");
       System.out.println("hello Git");
       System.out.println("hello Git");
   }
